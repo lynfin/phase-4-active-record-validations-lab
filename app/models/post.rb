0c294362543ac884ee7de4c69045145fc4ef20fd
Lyn <lynfinman@gmail.com>
2022-07-19 16:45:27 -0500

    commit tree 64eadc79e30ac6a4b3220832fb538aa1e899a154

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait_title
+
+  def clickbait_title
+    if title && !(title.include?("Won't Believe") || title.include?('Secret') || title.include?('Top ') || title.include?('Guess'))
+
+      errors.add(:title, 'not clickbait-y enough')
+    end
+  end
 end
